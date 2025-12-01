bda2edd5aede512de22543048b3aa9e6f9624a46
Deuteronome <o.burcker@e2c-grandlille.fr>
2025-12-01 16:39:05 +0100

    Début js

---
+++ script/test.js
@@ -0,0 +1,30 @@
+
+/*
+let username = "Misterbear"
+let number = 25 //integer
+let double = 12.59 //double
+let boolean = true // ou false
+
+console.log(username, number, double, boolean); 
+
+let number1 = 58
+let number2 = number1 + number 
+
+console.log(number1 + number2)
+console.log(number1 -number2)
+console.log(number1 * number2)
+console.log(number1 / number2)
+console.log(number1 % number2)
+
+username = "Deuty"
+
+console.log(username)
+*/
+let firstname;
+let lastname;
+
+firstname = prompt("Quel et votre prénom?");
+lastname = prompt("Quel est votre patronyme?");
+
+alert(`Bonjour ${firstname} ${lastname}, comment vas tu?`)
+
