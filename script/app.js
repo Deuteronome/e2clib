b4ed63c23c191f8783e4cb303b93679a0c5638cb
Deuteronome <o.burcker@e2c-grandlille.fr>
2025-12-03 11:51:46 +0100

    js connect

--- script/app.js
+++ script/app.js
@@ -1,7 +1,13 @@
 //Définir les variable - récuperer les élements html dont on a besoin
 const connectButton = document.querySelector("#connect-button");
 const main = document.querySelector("main");
+const connectBox = document.querySelector("#connect-background");
+console.log(connectBox);
+const closeButton = document.querySelector(".close-button");
+const formBox = document.querySelector("#connect-form");
 let count = 0;
+
+
 //Définir les fonctions - les actions possibles
 function addText() {
     count ++;
@@ -15,10 +21,28 @@ function addText() {
     if(count <= 10) {
         newText.innerHTML = `tentative N° ${count}`
     } else {
-        newText.innerHTML = "C'est pas fini"
+        newTxt.innerHTML = "C'est pas fini"
         connectButton.removeEventListener("click", addText)
     }
     main.appendChild(newText)    
 }
+
+function openConnect() {
+    connectBox.style.display = "flex"
+    connectBox.style.opacity = "1"
+}
+
+function closeConnect() {
+    connectBox.style.display = "none"
+    connectBox.style.opacity = "0"
+}
+
+function noClose(event) {
+    event.stopPropagation()
+}
+
 //mettre en place les déclencheurs - écouteur d'évenements</p>
-connectButton.addEventListener("click", addText)
+connectButton.addEventListener("click", openConnect)
+closeButton.addEventListener("click",closeConnect)
+connectBox.addEventListener("click",closeConnect)
+formBox.addEventListener("click", noClose)
